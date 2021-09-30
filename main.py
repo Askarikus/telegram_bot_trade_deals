eac01eb12b4686ee9125355089dd497959692dff
Askarikus <askarikus@yandex.ru>
2021-09-30 11:49:13 +0500

    Start telegram_bot organikus t.me/organikus_bot

---
+++ main.py
@@ -0,0 +1,15 @@
+import telebot
+from auth_data import token
+
+
+def telegram_bot(token):
+    bot = telebot.TeleBot(token)
+
+    @bot.message_handler(commands="start")
+    def start_message(message):
+        bot.send_message(message.chat.id, "Hello")
+
+    bot.polling()
+
+if __name__ == "__main__":
+    telegram_bot(token)
